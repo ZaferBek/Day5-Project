1ee62267af3b2c0fff965033971fbbb37ff12d1a
Zafarbek Husainov <72046847+ZaferBek@users.noreply.github.com>
2020-10-13 18:43:12 +0100

    Commit to create conflict

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello EU3, MY COWORKER ADDED AWESOME HELLO "); //added some comment // some simulation
-        System.out.println("Adding some more line in local");
+        System.out.println("Adding some more line in local,
+                           Lets add some code to create conflict between local and remote");
+        
 
     }
 }
