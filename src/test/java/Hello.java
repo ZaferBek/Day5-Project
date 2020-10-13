44a6ac25f58eec6f329a67d7a2326b6ac88937b5
Zafarbek Husainov <72046847+ZaferBek@users.noreply.github.com>
2020-10-13 18:40:32 +0100

    Coworker added some commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello EU3"); //added some comment
+        System.out.println("Hello EU3, MY COWORKER ADDED AWESOME HELLO "); //added some comment // some simulation
         System.out.println("Adding some more line in local");
 
     }
