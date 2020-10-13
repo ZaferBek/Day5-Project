9cae3b10765d0dc4283ca1e76672d6a57d0c4efd
Zafarbek Husainov <72046847+ZaferBek@users.noreply.github.com>
2020-10-13 18:48:13 +0100

    Remote change

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello EU3, MY COWORKER ADDED AWESOME HELLO "); //added some comment // some simulation
         System.out.println("Adding some more line in local, Adding some more awesome commits");
-        System.out.println("Lets see the git hub");
+        System.out.println("Lets see the git hub, DOING SOME CHANGES FROM REMOTE ");
         
 
     }
