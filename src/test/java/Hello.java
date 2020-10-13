57186fcb6b9227906b19066d4167b64bd1b41f57
ZaferBek <zaferbek22@gmail.com>
2020-10-13 18:22:17 +0100

    Changes on Hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello EU3");
 
+        System.out.println("Lets do some changes here");
+        System.out.println("Add some lines of code");
+
     }
 }
