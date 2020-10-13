5bb16d6069649aa9e7d2742cc8f255c3308db1aa
ZaferBek <zaferbek22@gmail.com>
2020-10-13 17:33:07 +0100

    Some more code

--- src/test/java/Feature1Work.java
+++ src/test/java/Feature1Work.java
@@ -1,5 +1,7 @@
 public class Feature1Work {
     public static void main(String[] args) {
         System.out.println("Feature 1 has been developed");
+        //some code are added
+        //to publish in github
     }
 }
