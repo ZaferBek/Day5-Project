a3631214222566f0813f1fe153b043af1a360473
ZaferBek <zaferbek22@gmail.com>
2020-10-13 17:47:05 +0100

    Feature2 socond commit

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -2,5 +2,8 @@ public class feature2 {
     public static void main(String[] args) {
 
         System.out.println("Working on feature 2");
+
+        //Lets add some more code
+        // and commit again
     }
 }
