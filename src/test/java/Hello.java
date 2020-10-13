fd1cc8c0492e2f8da605f400319bf4ea8ca03e01
ZaferBek <zaferbek22@gmail.com>
2020-10-13 18:32:39 +0100

    Added some commit on the same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello EU3");
+        System.out.println("Hello EU3"); //added some comment
+        System.out.println("Adding some more line in local");
 
     }
 }
