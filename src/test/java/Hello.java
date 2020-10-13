b546fc1d39ffaf4bf81fd77f3a116d73b092490e
Zafarbek Husainov <72046847+ZaferBek@users.noreply.github.com>
2020-10-13 18:26:21 +0100

    Added from git hub

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello EU3");
+        System.out.println("Hello FROM GIT HUB SOME CHANGES EU3");
 
     }
 }
